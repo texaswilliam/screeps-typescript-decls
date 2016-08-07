c34d5372ae961ead5a451eb4e27118cd74b46e08
William Pyburn <texas.william@gmail.com>
2016-08-07 00:43:30 -0500

    StructureSpawn.createCreep's memory param now takes a CreepMem instead of any.

--- StructureSpawn.d.ts
+++ StructureSpawn.d.ts
@@ -6,7 +6,7 @@ declare class StructureSpawn extends OwnedStructure implements HasEnergy {
     spawning?: { name: CreepName; needTime: number; remainingTime: number; };
 
     canCreateCreep(body: BodyPartType[], name?: string): ErrCode;
-    createCreep(body: BodyPartType[], name?: string, memory?: any): ErrCode;
+    createCreep(body: BodyPartType[], name?: string, memory?: CreepMem): ErrCode;
     recycleCreep(target: Creep): ErrCode;
     renewCreep(target: Creep): ErrCode;
     /** @deprecated */
